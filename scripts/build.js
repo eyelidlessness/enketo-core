c7292bf3381beee40944b393169c518787b41bae
eyelidlessness <trevor.enketo.dev@gmail.com>
2023-06-07 11:34:00 -0700

    Include local Roboto font for new theme

--- scripts/build.js
+++ scripts/build.js
@@ -1,16 +1,25 @@
 const alias = require('esbuild-plugin-alias');
 const esbuild = require('esbuild');
+const fs = require('fs');
 const path = require('path');
 const pkg = require('../package.json');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
+const cwd = process.cwd();
+
 const aliases = Object.fromEntries(
     Object.entries(pkg.browser).map(([key, value]) => [
         key,
-        path.resolve(process.cwd(), value).replace(/(\.js)?$/, '.js'),
+        path.resolve(cwd, value).replace(/(\.js)?$/, '.js'),
     ])
 );
+
+const fontBasePath = path.resolve(cwd, './src/fonts/Roboto');
+const fontPaths = fs
+    .readdirSync(fontBasePath)
+    .filter((fileName) => fileName.endsWith('.woff'))
+    .map((fileName) => path.resolve(fontBasePath, fileName));
 
 esbuild.build({
     assetNames: '[name]',
@@ -19,12 +28,14 @@ esbuild.build({
         'src/index.html',
         'app.js',
         'src/images/odk-logo.svg',
+        ...fontPaths,
     ],
     format: 'iife',
     loader: {
         '.html': 'file',
         '.png': 'file',
         '.svg': 'file',
+        '.woff': 'file',
     },
     minify: isProduction,
     outdir: 'build',
